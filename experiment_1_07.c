22021e999134041bf00e5848ab5d71f5e3d75b54
PearlBuha123 <pearl.2506207201@vcet.edu.in>
2026-07-22 11:41:21 +0530

    Add files via upload

---
+++ experiment_1_07.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+
+// Function to print the array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+// Function to perform Selection Sort
+void selectionSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        int min_idx = i;
+        for (int j = i + 1; j < size; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        // Swap the found minimum element with the first element
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+// Function to perform Insertion Sort
+void insertionSort(int arr[], int size) {
+    for (int i = 1; i < size; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Move elements of arr[0..i-1] that are greater than key
+        // to one position ahead of their current position
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int size, choice;
+
+    // Get array size from user
+    printf("Enter the number of elements in the array: ");
+    if (scanf("%d", &size) != 1 || size <= 0) {
+        printf("Invalid array size.\n");
+        return 1;
+    }
+
+    int arr[size];
+
+    // Get array elements from user
+    printf("Enter %d elements:\n", size);
+    for (int i = 0; i < size; i++) {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    //select insertion or selection
+    printf("\nSelect Sorting Method:\n");
+    printf("1. Selection Sort\n");
+    printf("2. Insertion Sort\n");
+    printf("Enter your choice (1 or 2): ");
+    scanf("%d", &choice);
+
+    printf("\nOriginal array: ");
+    printArray(arr, size);
+
+    // Execute chosen sorting method
+    switch (choice) {
+        case 1:
+            selectionSort(arr, size);
+            printf("Sorted array (Selection Sort): ");
+            printArray(arr, size);
+            break;
+        case 2:
+            insertionSort(arr, size);
+            printf("Sorted array (Insertion Sort): ");
+            printArray(arr, size);
+            break;
+        default:
+            printf("Invalid choice! Array was not sorted.\n");
+            break;
+    }
+
+    return 0;
+}
